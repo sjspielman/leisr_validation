eba054e97263538922b10f02256e1279d2b29625
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2018-01-15 12:32:37 -0500

    dependencies made easier

---
+++ install_R_requirements.R
@@ -0,0 +1,8 @@
+#############################################################
+############# Install R package dependencies ################
+#############################################################
+
+
+packages.install("tidyverse")
+packages.install("broom")
+packages.install("cowplot")
